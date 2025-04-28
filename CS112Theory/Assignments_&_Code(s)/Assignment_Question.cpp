56635b8f1488a92fe5b7c7840f0522b3c3bba89c
CodeBySRY <shayan.yazdanie@gmail.com>
2025-04-29 00:23:35 +0500

    assignment question added

---
+++ CS112Theory/Assignments_&_Code(s)/Assignment_Question.cpp
@@ -0,0 +1,59 @@
+/*
+Spring 2025
+CS112
+Assignment A
+
+Q1. Write a recursive function sum_of_digits() that takes a single integer as input. The
+function returns the sum of all digits in the integer input. For example, if input is 175, the
+function returns 13 (1+7+5=13). Write a program to demonstrate correct execution of the
+function sum_of_digits().
+
+Q2. Convert the following code into a program. Run the program and explain the output
+displayed by the program.
+
+void some_function(void);
+int x = 5;
+int main() {
+    int x=20;
+    cout<<" "<< x << endl;
+    { int x=66;
+    cout<<" "<< x << endl;
+    }
+    some_function();
+    some_function();
+    { int y=66;
+    cout<<" "<< x+y << endl;
+    }
+}
+void some_function(void) {
+    static int z = 10;
+    cout<<" "<< x+z << endl;
+    if (z < 11) {
+        z++; ++z; x=x*2;
+    }
+}
+
+Q3. Convert the following code into a program. Run the program and then correct the
+program to display correct output. Explain the output before & after correction.
+
+enum Color { RED, GREEN, BLUE };
+typedef struct {
+    int x;
+    int y;
+    Color pixelColor;
+} Pixel;
+void printPixel(Pixel p) {
+    const char* colorNames[] = {"BLUE", "GREEN", "RED" };
+    cout<<" The pixel at "<<p.x<<","<<p.y<<" is "<< colorNames[p.pixelColor]<<endl;
+}
+int main() {
+    Pixel p1, p2, p3;
+    p1.x = 10; p1.y = 10; p1.pixelColor = RED;
+    p2.x = 20; p2.y = 20; p2.pixelColor = GREEN;
+    p3.x = 30; p3.y = 30; p3.pixelColor = BLUE;
+    printPixel(p1);
+    printPixel(p2);
+    printPixel(p3);
+    return 0;
+}
+*/
