8a49ec60ecbb69312d6e543d501da31caee8cda8
CodeBySRY <shayan.yazdanie@gmail.com>
2025-07-15 02:07:43 +0500

    operator overloading logic

--- CS112 Theory/Concepts/Standard Template Library/min_max_element.cpp
+++ CS112 Theory/Concepts/Standard Template Library/min_max_element.cpp
@@ -2,6 +2,7 @@
 #include <algorithm> // to use the minmax_element function();
 #include <vector>
 #include <iomanip>  // For setw()
+#include <string>
 
 /* The minmax_element() function: Return smallest and largest elements in range
 Returns a pair with an iterator pointing to the element with the smallest value in the range [first,last) as first element, and the largest as second.
@@ -21,6 +22,16 @@ class Student {
         void SearchStudent(const vector<Student>&s);
         Student(string s, int m);
 };
+
+ // Whenever two objects will be compared, they will be compared on the basis on the marks.
+bool operator<(const Student& a, const Student& b) {
+    return a.getMarks() < b.getMarks();
+}
+
+ostream& operator<<(ostream& out, const Student& s) {
+    out << s.getName() << " - " << s.getMarks();
+    return out;
+} 
 
 Student::Student(string s, int m) : name(s), marks(m) {}
 
@@ -61,18 +72,12 @@ if (found == false) {
 }
 
 void MinMaxMarks(vector<Student>&s1) {
-    vector<int> stuMarks;
-   for (const Student& s : s1) {
-        int marks = s.getMarks();
-        stuMarks.push_back(marks);
-
-   } 
-   
-   auto output = minmax_element(stuMarks.begin(), stuMarks.end());
-   cout << "The Maximum Marks = " << *output.second << endl;
-   cout << "The Minimum Marks = " << *output.first << endl;
-
+    auto output = minmax_element(s1.begin(), s1.end());
+    cout << "Highest Score: " << *output.second << endl;
+    cout << "Lowest Score: " << *output.first << endl;
 }
+
+
 
 int main() {
     Student s(" ", 0);
